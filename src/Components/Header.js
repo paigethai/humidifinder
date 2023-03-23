cc134a89649b58d96bdc10a64f6d6cd3bf3be7f0
Paige Thai <paigegiathai@gmail.com>
2023-03-22 23:18:37 -0400

    added header component

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div className="title-container">
+                <h1>Sneezon</h1>
+                <p>Stay Ahead of Your Sneezes: Your Personalized Pollen Forecast</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
